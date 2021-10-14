1a0432814b45297a5fd3716c3134372adb6b370e
Nathan <nsrirama@smu.edu>
2021-10-14 17:18:10 -0500

    added views and base viewcontrollers

--- MSL-Assignment-three/ViewController.swift
+++ MSL-Assignment-three/ViewController.swift
@@ -6,9 +6,15 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    let defaults = UserDefaults.standard
+    
+    let activityManager = CMMotionActivityManager()
+    let pedometer = CMPedometer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ MSL-Assignment-three/ViewControllerTwo.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerTwo.swift
+//  MSL-Assignment-three
+//
+//  Created by UbiComp on 10/13/21.
+//
+
+import UIKit
+
+
+class ViewControllerTwo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
